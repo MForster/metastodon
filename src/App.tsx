742798de3274fec1997b7fbc580911ea1b31cd39
Michael Forster <email@michael-forster.de>
2022-11-21 08:32:19 +0100

    Extract a `LoginButton` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { useEffect, useState } from 'react'
 import Instance from './instance'
+import LoginButton from './LoginButton'
 import Status from './Status'
-import PostView from './StatusCard'
+import StatusCard from './StatusCard'
 
 export default function App() {
   const instance = Instance.instances()[0] // TODO: multiple instances
@@ -12,34 +13,9 @@ export default function App() {
     instance?.get('api/v1/timelines/home').then(setTimeline)
   }, [])
 
-  const [loginDialogOpen, setLoginDialogOpen] = useState(false)
-  const handleLogin = () => {
-    setLoginDialogOpen(false)
-    const instance_name = (document.getElementById('instance') as HTMLInputElement).value
-    new Instance(instance_name).beginLogin()
-  }
-  useEffect(Instance.maybeFinishLogin, [])
-
   return <>
-    <Typography sx={{ display: 'inline' }}>
-      {instance?.get_account_name()}
-    </Typography>
-    <Button onClick={() => setLoginDialogOpen(true)}>Login</Button>
-    <Dialog open={loginDialogOpen} onClose={() => setLoginDialogOpen(false)}>
-      <DialogTitle>Login</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Please enter the name of the instance you want to log into, for example "mastodon.social".
-        </DialogContentText>
-        <TextField autoFocus id="instance" label="Instance" type="url" fullWidth variant="standard" />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => setLoginDialogOpen(false)}>Cancel</Button>
-        <Button onClick={handleLogin}>Login</Button>
-      </DialogActions>
-    </Dialog>
-    {
-      timeline.map((status) =>
-        <PostView key={status.uri} status={status} />)
-    } </>
+    <Typography sx={{ display: 'inline' }}>{instance?.get_account_name()}</Typography>
+    <LoginButton />
+    {timeline.map(status => <StatusCard key={status.uri} status={status} />)}
+  </>
 }
---
+++ src/LoginButton.tsx
@@ -0,0 +1,32 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from '@mui/material'
+import { useEffect, useState } from 'react'
+import Instance from './instance'
+
+export default function LoginButton() {
+  useEffect(Instance.maybeFinishLogin, [])
+
+  const [open, setOpen] = useState(false)
+  const handleLogin = () => {
+    setOpen(false)
+    const instanceInput = document.getElementById('instance') as HTMLInputElement
+    new Instance(instanceInput.value).beginLogin()
+  }
+
+  return <>
+    <Button onClick={() => setOpen(true)}>Login</Button>
+
+    <Dialog open={open} onClose={() => setOpen(false)}>
+      <DialogTitle>Login</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please enter the name of the instance you want to log into, for example "mastodon.social".
+        </DialogContentText>
+        <TextField autoFocus id="instance" label="Instance" type="url" fullWidth variant="standard" />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setOpen(false)}>Cancel</Button>
+        <Button onClick={handleLogin}>Login</Button>
+      </DialogActions>
+    </Dialog>
+  </>
+}
