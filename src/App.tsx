ab0cf76b41e6fba63e96ffe2cc809d9ab620d59d
Michael Forster <email@michael-forster.de>
2022-11-25 07:43:45 +0100

    Move database initialisation to `Main.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import { Box, Button, Stack, Typography } from '@mui/material'
 import useEventListener from '@use-it/event-listener'
-import { useEffect, useState } from 'react'
-import { Database, DBContext } from './Database'
+import { useContext, useEffect, useState } from 'react'
+import { DBContext } from './Database'
 import Instance from './Instance'
 import LoginButton from './LoginButton'
 import Timeline from './Timeline'
 
 export default function App() {
   let [instances, setInstances] = useState(Instance.instances())
-  let db = new Database()
 
   useEventListener('accounts-changed', () => setInstances(Instance.instances()))
   useEffect(() => instances.forEach(handleRefresh), [instances])
 
+  const db = useContext(DBContext)
   const handleRefresh = (instance: Instance) => {
     instance.get('api/v1/timelines/home').then(db.putStatuses.bind(db))
   }
 
-  return <DBContext.Provider value={db}>
+  return <>
     <Stack direction="row" mx={4} spacing={4}>
       <LoginButton />
       {instances.map(instance => <Box key={instance.getName()}>
@@ -27,6 +27,5 @@ export default function App() {
       </Box>)}
     </Stack>
     <Timeline />
-  </DBContext.Provider>
-
+  </>
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Database, DBContext } from './Database'
 import './Main.css'
 
 import '@fontsource/roboto/300.css'
@@ -8,8 +9,14 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+function DBProvider({ children }: { children: React.ReactNode }) {
+  return <DBContext.Provider value={new Database()}>{children}</DBContext.Provider>
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <DBProvider>
+      <App />
+    </DBProvider>
   </React.StrictMode>
 )
